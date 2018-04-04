b5c8599782fb6229ee62b03d3a7234fff38a9100
cjh <cjh_android@163.com>
2018-04-04 11:16:40 +0800

    fix Denied starting an intent without a user gesture cjh

--- app/src/main/java/study/com/studyh5/activity/MainActivity.java
+++ app/src/main/java/study/com/studyh5/activity/MainActivity.java
@@ -1,18 +1,24 @@
 package study.com.studyh5.activity;
 
+import android.annotation.SuppressLint;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import study.com.studyh5.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        WebView webView = findViewById(R.id.webView);
-        webView.loadUrl("file:///android_asset/html/welcome.html");
+
     }
 }
--- app/src/main/java/study/com/studyh5/base/BaseActivity.java
+++ app/src/main/java/study/com/studyh5/base/BaseActivity.java
@@ -3,4 +3,8 @@ package study.com.studyh5.base;
 import android.support.v7.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
+
+
+    
+
 }
